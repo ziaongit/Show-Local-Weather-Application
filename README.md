3a3fe0a75b94fc010776430a65142ce60aee5499
Zia <zia_gt@yahoo.com>
2017-07-29 03:07:08 +0200

    weather api added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Show Local Weather Application
 With this application, the user can see the weather in my current location. The user can see a different icon and background image (e.g. snowy mountain, hot desert) depending on the weather. The user can push a button to toggle between Fahrenheit and Celsius.
+
+### Note
+For development purpose windows chrome users must past 
+chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
+in run and press enter, to open chrome in unsecure mode. 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 (function WeatherApplication(){
     this.init =function() {
         this.startApp();
@@ -11,33 +10,83 @@ this.startApp = function(){
     var temperature = document.getElementById('temperature');
     var loc = document.getElementById('loc');
     var icon = document.getElementById('icon');
+    var description = document.getElementById('description');
     var humidity = document.getElementById('humidity');
     var wind = document.getElementById('wind');
     var direction = document.getElementById('direction');
-
-    var weather = {};
-    weather.temperature = '45';
-    weather.loc = 'Stockholm';
-    weather.icon = 200;
-    weather.humidity = 35;
-    weather.wind = 3.5;
-    weather.direction = 'N';
     
-    update(weather);
+
+    updateByLaLo();
+    
+    //update(weather);
 };
 
 /* --------------------------
      START APP END
 --------------------------- */
 
+/* --------------------------
+     UPDATE BY ZIP START
+--------------------------- */
+this.updateByLaLo = function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            console.log('Geolocation is not supported by this browser.');
+        }
+    
+    function showPosition(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=dc6951009c69e18093fa15d39678b152';
+        sendRequest(url);
+        }
+}
+/* --------------------------
+     UPDATE BY ZIP END
+--------------------------- */
+
+/* --------------------------
+     SEND REQUEST START
+--------------------------- */
+this.sendRequest = function(url) {
+    
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+            var data = JSON.parse(xhttp.responseText);
+            var weather = {};
+            weather.temperature = data.main.temp;
+            weather.loc = data.name;
+            weather.icon = data.weather[0].icon;
+            weather.description = data.weather[0].description;
+            weather.humidity = data.main.humidity;
+            weather.wind = data.wind.speed;
+            weather.direction = data.wind.deg;
+            update(weather);
+        }else {
+          console.log('Error: ReadyState:'+xhttp.readyState+' status:'+xhttp.status);
+        }
+    };
+
+    xhttp.open('GET', url, true);
+    xhttp.send();
+}
+
+/* --------------------------
+     SEND REQUEST END
+--------------------------- */
 
 /* --------------------------
      UPDATE START
 --------------------------- */
-function update(weather) {
+this.update = function(weather) {
     temperature.innerHTML = weather.temperature;
     loc.innerHTML = weather.loc;
-    icon.src = 'resources/images/codes/'+weather.icon+'.png';
+    icon.src = 'http://openweathermap.org/img/w/'+weather.icon+'.png';
+    description.innerHTML = weather.description;
     humidity.innerHTML = weather.humidity;
     wind.innerHTML = weather.wind;
     direction.innerHTML = weather.direction;
@@ -45,6 +94,8 @@ function update(weather) {
 /* --------------------------
      UPDATE END
 --------------------------- */
+
+
 
 
 
